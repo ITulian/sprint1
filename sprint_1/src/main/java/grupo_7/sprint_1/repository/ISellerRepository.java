45efb3e7700416fe6d289fd3b0e4524bcbedc4f9
Nahuel Ignacio Quilpatay <nahuel.quilpatay@mercadolibre.com>
2024-02-19 10:21:52 -0300

    seller

--- sprint_1/src/main/java/grupo_7/sprint_1/repository/ISellerRepository.java
+++ sprint_1/src/main/java/grupo_7/sprint_1/repository/ISellerRepository.java
@@ -1,5 +1,8 @@
 package grupo_7.sprint_1.repository;
 
-public interface ISellerRepository {
+import grupo_7.sprint_1.dtos.PostPostDto;
+import grupo_7.sprint_1.entity.Post;
 
+public interface ISellerRepository {
+    Post postPost(int sellerId, PostPostDto newPost);
 }
--- sprint_1/src/main/java/grupo_7/sprint_1/repository/SellerRepositoryImp.java
+++ sprint_1/src/main/java/grupo_7/sprint_1/repository/SellerRepositoryImp.java
@@ -1,4 +1,35 @@
 package grupo_7.sprint_1.repository;
 
-public class SellerRepositoryImp {
+import grupo_7.sprint_1.dtos.PostPostDto;
+import grupo_7.sprint_1.entity.Post;
+import grupo_7.sprint_1.entity.Seller;
+import grupo_7.sprint_1.utils.Mapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class SellerRepositoryImp implements ISellerRepository {
+
+    Mapper mapper;
+    private List<Seller> sellers = new ArrayList<>();
+
+    public SellerRepositoryImp(Mapper mapper) {
+        this.mapper = mapper;
+    }
+
+    @Override
+    public Post postPost(int sellerId, PostPostDto newPost) {
+        Post post = mapper.convertPostDtoToPost(newPost);
+
+        Seller s = new Seller();
+        for (Seller se : sellers) {
+            if (se.getUserId() == sellerId) {
+                se.getPostList().add(post);
+            }
+        }
+
+        return post;
+    }
 }
