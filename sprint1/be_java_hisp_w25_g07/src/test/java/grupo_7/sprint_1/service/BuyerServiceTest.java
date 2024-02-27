d7b712d0c5d9ae61623d82ce1845cdc90db9188d
Nahuel Ignacio Quilpatay <nahuel.quilpatay@mercadolibre.com>
2024-02-27 12:19:20 -0300

    anotaciones tests

--- sprint1/be_java_hisp_w25_g07/src/test/java/grupo_7/sprint_1/service/BuyerServiceTest.java
+++ sprint1/be_java_hisp_w25_g07/src/test/java/grupo_7/sprint_1/service/BuyerServiceTest.java
@@ -1,4 +1,10 @@
 package grupo_7.sprint_1.service;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 public class BuyerServiceTest {
 }
