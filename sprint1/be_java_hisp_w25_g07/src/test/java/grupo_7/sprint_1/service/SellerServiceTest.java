4f29cff89715c3286bff42074649b2a44c6e6b3b
toalmada_meli <tomasfrancisco.almada@mercadolibre.com>
2024-02-27 18:26:33 -0300

    Test 006: Verificar correcto funcionamiento de orden por fecha asc y desc

--- sprint1/be_java_hisp_w25_g07/src/test/java/grupo_7/sprint_1/service/SellerServiceTest.java
+++ sprint1/be_java_hisp_w25_g07/src/test/java/grupo_7/sprint_1/service/SellerServiceTest.java
@@ -1,26 +1,43 @@
 package grupo_7.sprint_1.service;
 
+import grupo_7.sprint_1.dtos.PostDto;
 import grupo_7.sprint_1.dtos.SellerDTO;
+import grupo_7.sprint_1.entity.Buyer;
+import grupo_7.sprint_1.entity.Seller;
 import grupo_7.sprint_1.exception.BadRequestException;
+import grupo_7.sprint_1.repository.IBuyerRepository;
+import grupo_7.sprint_1.repository.ISellerRepository;
+import grupo_7.sprint_1.utils.MockBuilder;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.testng.Assert;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.when;
+import java.time.LocalDate;
+import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
 public class SellerServiceTest {
 
     @Mock
     SellerServiceImp sellerServiceImp;
+
+    @Mock
+    IBuyerRepository buyerRepository;
+
+    @Mock
+    ISellerRepository sellerRepository;
+
 
     @Test
     @DisplayName("T-0005: Verificar que el tipo de ordenamiento por fecha exista (US-0009) - Éxito")
@@ -59,5 +76,51 @@ public class SellerServiceTest {
         SellerDTO actual = sellerServiceImp.cantidadSeguidores(userId);
 
         Assert.assertEquals(expected.getFollowerCount(), actual.getFollowerCount());
+    }
+
+    @Test
+    @MockitoSettings(strictness = Strictness.LENIENT)
+    @DisplayName("T-0006: Verificar que el tipo de ordenamiento por fecha ascendente sea correcto (US-0008)")
+    public void verifyDateAscFilterIsCorrect() {
+        Integer buyerId = 1;
+        String order = "date_asc";
+        List<Seller> sellers = MockBuilder.mockSellers();
+        Buyer buyer = MockBuilder.mockBuyerForSeller();
+
+        when(buyerRepository.findBuyerById(buyerId)).thenReturn(buyer);
+        when(sellerRepository.getAllSellers()).thenReturn(sellers);
+
+        List<PostDto> posts = sellerServiceImp.getRecentPostsFromFollowedSellers(buyerId, order);
+
+        LocalDate lastDate = null;
+        for(PostDto post : posts) {
+            if(lastDate != null) {
+                assertTrue(post.getDate().isAfter(lastDate));
+            }
+            lastDate = post.getDate();
+        }
+    }
+
+    @Test
+    @MockitoSettings(strictness = Strictness.LENIENT)
+    @DisplayName("T-0006: Verificar que el tipo de ordenamiento por fecha descendente sea correcto (US-0008)")
+    public void verifyDateDescFilterIsCorrect() {
+        Integer buyerId = 1;
+        String order = "date_desc";
+        List<Seller> sellers = MockBuilder.mockSellers();
+        Buyer buyer = MockBuilder.mockBuyerForSeller();
+
+        when(buyerRepository.findBuyerById(buyerId)).thenReturn(buyer);
+        when(sellerRepository.getAllSellers()).thenReturn(sellers);
+
+        List<PostDto> posts = sellerServiceImp.getRecentPostsFromFollowedSellers(buyerId, order);
+
+        LocalDate lastDate = null;
+        for(PostDto post : posts) {
+            if(lastDate != null) {
+                assertTrue(post.getDate().isBefore(lastDate));
+            }
+            lastDate = post.getDate();
+        }
     }
 }
--- sprint1/be_java_hisp_w25_g07/src/test/java/grupo_7/sprint_1/utils/MockBuilder.java
+++ sprint1/be_java_hisp_w25_g07/src/test/java/grupo_7/sprint_1/utils/MockBuilder.java
@@ -1,7 +1,11 @@
 package grupo_7.sprint_1.utils;
 
 import grupo_7.sprint_1.entity.Buyer;
+import grupo_7.sprint_1.entity.Post;
+import grupo_7.sprint_1.entity.Product;
+import grupo_7.sprint_1.entity.Seller;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,5 +16,23 @@ public class MockBuilder {
                 "Buyer_1",
                 List.of()
         );
+    }
+
+    public static List<Seller> mockSellers() {
+        List<Seller> sellers = new ArrayList<>();
+        Seller seller = new Seller();
+        seller.setUserId(1);
+        seller.setFollowers(List.of(new Buyer()));
+        seller.setPosts(List.of(new Post(new Product(), 1, 100.0, LocalDate.now().minusDays(2)),
+                new Post(new Product(), 2, 200.0, LocalDate.now().minusDays(3)),
+                new Post(new Product(), 3, 300.0, LocalDate.now().minusDays(4))));
+        sellers.add(seller);
+        return sellers;
+    }
+
+    public static Buyer mockBuyerForSeller(){
+        Seller seller = new Seller();
+        seller.setUserId(1);
+        return new Buyer(1,"Buyer_1",List.of(seller));
     }
 }
